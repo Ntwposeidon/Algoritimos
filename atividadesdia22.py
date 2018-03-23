5204ab68714a6243b3de2ba661eb5ae52d8b17dd
Ntwposeidon <jhonispfc@live.com>
2018-03-22 22:11:55 -0300

    criação do projeto

---
+++ atividadesdia22.py
@@ -0,0 +1,87 @@
+# 1 Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+#valor = int(input("digite um valor"))
+#if valor > 0:
+    #print("valor positivo")
+#else:
+    #print("valor negativo")
+
+# 2 Faça um Programa que peça dois números e imprima o maior deles.
+#n1 = int(input("digite um numero: "))
+#n2 = int(input("digite um outro numero: "))
+#if n1 >= n2:
+    #print("numero maior")
+#else:
+    #print("numero menor")
+
+# 3 Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+#valor1 = (input("digite um valor: "))
+#valor2 = (input("digite um valor: "))
+#alor3 = (input("digite um valor: "))
+#soma = ("valor1 + valor2")
+
+#if valor1+valor2 <= valor3:
+      #print("valor maior")
+#else:
+      #print("valor menor")
+
+# 4 Faça um Programa que verifique o sexo de uma pessoa. O usuário deve informar ‘F’ ou ‘M’ e o programa deve escrever “Feminino” ou “Masculino”, conforme a letra digitada.
+#sexo = input("digite uma letra: ")
+#if sexo == "f":
+  #print("feminino")
+#elif sexo == "m":
+  #print("masculino")
+#else:
+  #print("sexo inválido")
+
+
+# 5 Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar.
+#Dica: utilize o operador módulo (resto da divisão).
+#n = int(input("digite um numero"))
+#if n % 2 == 0:
+     #print("numero par")
+#else:
+    #print("numero impar")
+
+# 6 Faça um algoritmo que peça um número e se ele for par some 5, se não, some 8.
+#n = int(input("digite um numero"))
+#if n % 2 == 0:
+     #print(n+5)
+#else:
+    #print(n+8)
+
+# 7 Crie um programa que peça uma nota de trabalho e uma de prova (as duas de 0 a 100).
+#Se a média aritmética das notas for maior ou igual a 60, escreva “Aprovado”, se não, “Reprovado”.
+#nota1 = float(input("digite a nota do trabalho"))
+#nota2 = float(input("digite a nota da prova"))
+#n = nota1 + nota2 / 2
+#if n >= 60:
+    #print("Aprovado")
+#else:
+    #print("Reprovado")
+
+# 8  Construa um programa que recebe três valores, A, B e C.
+#Em seguida, apresente na tela somente o maior deles.
+#A = int(input("digite valor de A: "))
+#B = int(input("digite valor de B: "))
+#C = int(input("digite valor de C: "))
+
+#if A > B and A > C:
+    #print(" A maior valor")
+#elif B > C and B > A:
+    #print("B maior valor")
+#else:
+    #C > B and C > A
+    #print("C maior valor")
+# 9 Construa um programa que recebe três valores, A, B e C. Em seguida,
+#apresente na tela os números em ordem crescente.
+#A = int(input("digite valor A: "))
+#B = int(input("digite valor B: "))
+#C = int(input("digite valor C: "))
+
+#if A > B and A > C :
+    #print("Valor A")
+#elif B > C and B > A :
+    #print("Valor B")
+#else:
+    #C > B and C > A
+    #print("Valor C")
